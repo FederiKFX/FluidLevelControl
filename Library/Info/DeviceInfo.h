d12406de7f66a986bfea26aabf2694b993c67498
Oleh <oleh.yatskiv@gmail.com>
2022-06-11 14:57:36 +0300

    Add json work to DeviceInfo

--- Library/Info/DeviceInfo.h
+++ Library/Info/DeviceInfo.h
@@ -7,8 +7,28 @@ struct Device
     Colour fluidType;
     int fullness;
     std::vector<bool> sensors;
-    std::vector<bool> pins;
+    std::vector<bool> pins; 
 };
+
+void from_json(const nlohmann::json& j, Device& d)
+{
+    j.at("name").get_to(d.name);
+    j.at("fluidType").get_to(d.fluidType);
+    j.at("fullness").get_to(d.fullness);
+    j.at("sensors").get_to(d.sensors);
+    j.at("pins").get_to(d.pins);
+}
+
+void to_json(nlohmann::json& j, const Device& d)
+{
+    j = nlohmann::json{
+        { "name", d.name },
+        { "fluidType", d.fluidType },
+        { "fullness", d.fullness },
+        { "sensors", d.sensors },
+        { "pins", d.pins }
+    };
+}
 
 class DeviceInfo : public Info
 {
